c239f3d9d6936298175193a63d11b0fbfb9bdeba
0x8BADC0DE <92804442+0x8BADC0DE@users.noreply.github.com>
2022-06-20 17:05:57 +0200

    Form update

--- python/flask/static/js/script.js
+++ python/flask/static/js/script.js
@@ -1,38 +1,23 @@
-class Point{
+const pointsDiv = $("#points");
+let pointsCount = pointsDiv.children().length
+$("#append-point").on("click", function () { 
+    pointsCount += 1;
     /**
-    * @param {Number} index
-    * @param {HTMLDivElement} divPoints
-    */
-    constructor(index, divPoints){
-        /**
-         * @type {Number}
-         */
-        this.index = index;
-        /**
-         * @type {JQuery|HTMLDivElement}
-         */
-        this.parentDiv = divPoints;
-        /**
-         * @type {JQuery|HTMLDivElement}
-         */
-        this.div = $(`<div id=point-${this.index}></div>`);
-        /**
-         * @type {JQuery|HTMLInputElement}
-         */
-        this.xInput = $(`<input type="number" name="point-${this.index}-x>`);
-        this.xInput.appendTo(this.div);
-        /**
-         * @type {JQuery|HTMLInputElement}
-         */
-        this.yInput = $(`<input type="number" name="point-${this-index}-y`);
-        this.yInput.appendTo(this.div);
+     * @type {HTMLDivElement}
+     */
+    pointDiv = $(`<div id="point-${pointsCount}"></div>`);
+    xInput = $(`<input type="number" placeholder="x value...">`);
+    yInput = $(`<input type="number" placeholder="y value...">`);
+    pointDiv.append(xInput);
+    pointDiv.append(yInput);
+    pointsDiv.append(pointDiv);
 
-        this.appendButton = $(`<input type="button" value="Rajouter un ${index}ème point">`);
-        this.appendButton.on("click", function(){Point(index+1, divPoints)})
-        this.appendButton.appendTo(this.div);
-
-        this.removeButton = $(`<input type="button" value="Supprimer ce point"`);
-        this.removeButton.on("click", function(){$(`#point-${index}`).remove()});
-        this.removeButton.appendTo(this.div);
+    // Add point..
+})
+$("#remove-point").on("click", function () {
+    if(pointsCount > 0)
+    {
+        $(`#point-${pointsCount}`).remove();
+        pointsCount -= 1;
     }
-}
+  })
