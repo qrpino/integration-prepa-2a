9d79173d5933d9113b6475c3808bb0222f585261
0x8BADC0DE <92804442+0x8BADC0DE@users.noreply.github.com>
2022-06-21 17:18:41 +0200

    update key

--- python/flask/main.py
+++ python/flask/main.py
@@ -12,13 +12,14 @@ def index():
         
         data['mode'] = request.form['mode'];
         if request.form.get('mode') == 0:
-            pass;
+            data["motors"] = []
+            for i in range(1, 5):
+                data["motors"].append(request.form["motor-"+str(i)])
         else:
             if int(request.form["points-count"]) > 0:
                 data["points"] = [];
                 for i in range(1, int(request.form["points-count"]) + 1):
                     data["points"].append((request.form["point-"+str(i)+"-x"], request.form["point-"+str(i)+"-y"]))
-                    pass;
         pass;
     elif(request.method == 'GET'):
         pass;
